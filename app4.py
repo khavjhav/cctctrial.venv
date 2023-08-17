58060c837caa76a34630d2f07f7637ddee31e315
Arnab Barua <ournob@outlook.com>
2023-08-17 16:44:28 +0600

    try later

--- app4.py
+++ app4.py
@@ -1,110 +1,98 @@
-from flask import Flask, render_template, Response
-import cv2
 import numpy as np
-import cvzone
-import math
+import cv2
+import time
+import onnxruntime
 from sort import *
-from ultralytics import YOLO
 
-app = Flask(__name__)
+# Load the ONNX model
+onnx_model_path = "yolo-weights\yolov8m.onnx"
+ort_session = onnxruntime.InferenceSession(onnx_model_path)
 
-# Initialize YOLO model, tracker, and other variables
-model = YOLO("yolov8n.pt")
-
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
-
-tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
-limitsUp = [(602, 147), (602, 866), (711, 968), (716, 137)]
-limitsDown = [(0, 0), (0, 200), (640, 200), (640, 0)]
-totalCountUp = []
-totalCountDown = []
-
-# OpenCV video capture
-cap = cv2.VideoCapture("rtsp://arnab:kh4vjh4v@103.205.180.214:554/Streaming/channels/1902")
+# Initialize video capture
+rtsp_url = "rtsp://arnab:kh4vjh4v@103.205.180.214:554/Streaming/channels/1902"
+cap = cv2.VideoCapture(rtsp_url)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-@app.route('/')
-def index():
-    return render_template('index2.html')
+# Tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.5)
+limitsUp = [(264, 185), (337, 188), (434, 356), (301, 354)]
+limitsDown = [(168, 201), (215, 193), (213, 359), (159, 357)]
 
-def gen_frames():
-    while True:
-        success, img = cap.read()
-        if not success:
-            break
+totalCountUp = []
+totalCountDown = []
 
-        # Process the frame
-        imgGraphics = cv2.imread("static/graphics.png", cv2.IMREAD_UNCHANGED)
-        overlay_width = imgGraphics.shape[1]
-        overlay_height = imgGraphics.shape[0]
-        new_overlay_width = int(overlay_width * 0.3)
-        new_overlay_height = int(overlay_height * 0.3)
-        resized_imgGraphics = cv2.resize(imgGraphics, (new_overlay_width, new_overlay_height))
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Mouse coordinates:", x, y)
 
-        img = cvzone.overlayPNG(img, resized_imgGraphics, (0, 0))
-        
-        # Your existing processing logic here...
-        results = model(img, stream=True)
-        detections = np.empty((0, 5))
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-            # Bounding Box
-             x1, y1, x2, y2 = box.xyxy[0]
-             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-             w, h = x2 - x1, y2 - y1
-            # Confidence
-             conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
-            cls = int(box.cls[0])
-            currentClass = classNames[cls]
-            if currentClass == "person" and conf > 0.2:
-                currentArray = np.array([x1, y1, x2, y2, conf])
-                detections = np.vstack((detections, currentArray))
-    resultsTracker = tracker.update(detections)
-    cv2.polylines(img, [np.array(limitsUp)], True, (0, 0, 255), 5)
-    cv2.polylines(img, [np.array(limitsDown)], True, (0, 0, 255), 5)
-    for result in resultsTracker:
-        x1, y1, x2, y2, id = result
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
-        cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
-                           scale=2, thickness=3, offset=10)
-        cx, cy = x1 + w // 2, y1 + h // 2
-        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-        if cv2.pointPolygonTest(np.array(limitsUp), (cx, cy), False) == 1:
-            if totalCountUp.count(id) == 0:
-                totalCountUp.append(id)
-                cv2.polylines(img, [np.array(limitsUp)], True, (0, 255, 0), 5)
-        if cv2.pointPolygonTest(np.array(limitsDown), (cx, cy), False) == 1:
-            if totalCountDown.count(id) == 0:
-                totalCountDown.append(id)
-                cv2.polylines(img, [np.array(limitsDown)], True, (0, 255, 0), 5)
-    cv2.putText(img, str(len(totalCountUp)), (200, 85), cv2.FONT_HERSHEY_PLAIN, 5, (139, 195, 75), 7)
-    cv2.putText(img, str(len(totalCountDown)), (450, 85), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 230), 7)
-    cv2.imshow("Image", cv2.resize(img, (0, 0), fx=1, fy=1))
-    cv2.waitKey(1)
+# Set up the windows
+cv2.namedWindow("Image")
+# Set the mouse callback function for the window
+cv2.setMouseCallback("Image", mouse_callback)
 
-    ret, buffer = cv2.imencode('.jpg', img)
-    frame = buffer.tobytes()
-    yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+frame_count = 0
+start_time = time.time()
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+    
+    # Preprocess the image for input to the ONNX model
+    input_name = ort_session.get_inputs()[0].name
+    # img_resized = cv2.resize(img, (640, 640))  # Resize the image to match model input size
+    img_resized = cv2.resize(img, (640, 480))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    img_input = img_resized.transpose(2, 0, 1).astype(np.float32) / 255.0
+    img_input = np.expand_dims(img_input, axis=0)  # Add batch dimension
+
+    # Perform inference using ONNX Runtime session
+    outputs = ort_session.run(None, {input_name: img_input})
+
+# Get the names of the output nodes
+    output_names = [output.name for output in ort_session.get_outputs()]
+
+# Print the output names and their shapes
+    for i, name in enumerate(output_names):
+     shape = ort_session.get_outputs()[i].shape
+     print(f"Output {i}: Name={name}, Shape={shape}")
+
+    # # Process the outputs (you need to adapt this based on your model's output format)
+    # # For example, if the output is a tensor containing bounding box coordinates and scores:
+    # boxes = outputs[0]  # Extract bounding box coordinates
+    # scores = outputs[1]  # Extract confidence scores
+    # class_ids = outputs[2]  # Extract class IDs
+    
+    # # Update tracker with detections
+    # detections = []
+    # for i, score in enumerate(scores[0]):
+    #     if score > 0.5:  # Set your desired confidence threshold here
+    #         x1, y1, x2, y2 = boxes[0][i]
+    #         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    #         detections.append([x1, y1, x2, y2, score])
+
+    # resultsTracker = tracker.update(np.array(detections))
+    
+    img_with_boxes = img.copy()
+
+    # for result in resultsTracker:
+    #     x1, y1, x2, y2, id = result
+    #     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    #     w, h = x2 - x1, y2 - y1
+    #     cv2.rectangle(img_with_boxes, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #     cv2.putText(img_with_boxes, f'ID: {int(id)}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # # Calculate and display FPS
+    # frame_count += 1
+    # elapsed_time = time.time() - start_time
+    # fps = frame_count / elapsed_time
+    # cv2.putText(img_with_boxes, f'FPS: {fps:.2f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    cv2.imshow("Image", cv2.resize(img_with_boxes, (0, 0), fx=1, fy=1))
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
