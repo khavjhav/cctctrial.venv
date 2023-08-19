7eda04982673809ca14e81d3df24adeec2f3c006
Niyaz Bin Hashem <niyazed@outlook.com>
2023-08-19 13:55:53 +0800

    feat: added UI and websocket to stream video

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
+COPY models models
+
 COPY src .
 
 CMD ["python3", "main.py"]
---
+++ src/app.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.templating import Jinja2Templates
+from src.utils.coco import COCO_CLASSES
+from ultralytics import YOLO
+from pathlib import Path
+import uvicorn
+import cv2
+import os
+
+app = FastAPI()
+
+video = "test_video.mp4"
+cap = cv2.VideoCapture(video)
+model = YOLO("./models/yolov8n.onnx")
+# model.export(format="onnx")
+
+prev_frame_time = 0
+new_frame_time = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
+
+@app.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
+
+@app.get('/base')
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/get-stream")
+async def get_stream(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            success, frame = cap.read()
+            frame = cv2.resize(frame, (640, 480))
+            results = model.predict(source=frame, show=False, stream=True)
+            for result in results:
+                boxes = result.boxes
+                
+                for box in boxes:
+                    class_name = COCO_CLASSES[int(box.cls[0])]
+                    
+                    if class_name != "person":
+                        continue
+                    
+                    bbox = box.xyxy[0]
+                    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+                    cv2.rectangle(frame, (x, y), (w, h), (100, 255, 0), 2)
+                    cv2.putText(frame, class_name, (x, y), font, 1, (100, 255, 0), 2)
+                    
+            if not success:
+                break
+            else:
+                _ , buffer = cv2.imencode('.jpg', frame)
+                await websocket.send_text("some text")
+                await websocket.send_bytes(buffer.tobytes())
+    except WebSocketDisconnect:
+        print("Client disconnected")   
+ 
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
